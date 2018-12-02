18c3cdaecf68a7619a81ad2ca734b4f5083671e7
Jordan McKible <jordan@mckible.com>
2018-12-02 09:51:12 -0800

    Set time zone

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,3 +20,5 @@ import { definitionsFromContext } from 'stimulus/webpack-helpers'
 const application = Application.start()
 const context = require.context('controllers', true, /.js$/)
 application.load(definitionsFromContext(context))
+
+require('../sprinkles/time_zone')
