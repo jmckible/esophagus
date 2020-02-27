18b76dbb9c35f07414e0d1f04f245e4743cd359e
Jordan McKible <jordan@mckible.com>
2020-02-27 07:24:01 -0800

    Empty sections; start ActionText

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,8 @@
 require('turbolinks').start()
 require('@rails/ujs').start()
 require('@rails/activestorage').start()
-require('@rails/actiontext').start()
+require('trix')
+require('@rails/actiontext')
 require('../sprinkles/time_zone')
 
 import 'controllers'
