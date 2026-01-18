af65f9174a2aff92d06f9f2e4fed102499bed98e
Jordan McKible <jordan@mckible.com>
2026-01-18 09:42:09 -0800

    Update bin/dev

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Vocal
+module Esophagus
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
